74c019211ff5401097c97cfd3609b78bfe7ab6dc
Olga Smirnova <almazkacode@gmail.com>
2024-10-16 12:39:14 +0500

    feat: add hover effects to interactive elements

--- src/components/elements/Card/Card.tsx
+++ src/components/elements/Card/Card.tsx
@@ -19,15 +19,13 @@ export const Card: React.FC<CardProps> = ({ firstName, lastName, avatar, email }
   };
 
   return (
-    <li>
-      <Link to="/profile" onClick={onClickCard}>
-        <div className={styles.card}>
-          <div className={styles.wrapper}>
-            <img className={styles.avatar} src={avatar} alt={firstName + ' ' + lastName} />
-          </div>
-          <p className={styles.name}>{firstName + ' ' + lastName}</p>
+    <Link to="/profile" onClick={onClickCard}>
+      <li className={styles.card}>
+        <div className={styles.wrapper}>
+          <img className={styles.avatar} src={avatar} alt={firstName + ' ' + lastName} />
         </div>
-      </Link>
-    </li>
+        <p className={styles.name}>{firstName + ' ' + lastName}</p>
+      </li>
+    </Link>
   );
 };
